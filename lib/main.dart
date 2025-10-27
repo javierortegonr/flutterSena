eaf2632fa83a02bffb56fb4c353762d77d077d5b
Javier Ortegon <javierortegonmp@gmail.com>
2025-10-26 21:52:37 -0500

    feat: add elememnts in counter_screen and change theme

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorSchemeSeed: Colors.blue),
       home: CounterScreen(),
     );
   }
